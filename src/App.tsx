578c2f61343f17440268c6c40d22ab81717f8154
maearon <manhng132@gmail.com>
2025-06-09 10:26:57 +0700

    add ValueStepper.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import ValueStepper from "./components/ValueStepper/ValueStepper";
+
 const App = () => {
   return (
     <div className="w-screen h-screen bg-neutral-950 flex items-center justify-center text-neutral-100">
       <div className="w-96 bg-neutral-800 p-4 rounded-lg">
-        // Your component go here
+        <ValueStepper />
       </div>
     </div>
   )
