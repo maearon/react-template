9282862f04dd1af5bbc5e4f4748ae1d58cf9581a
mark <manhng132@gmail.com>
2025-06-09 16:44:21 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@ import ValueStepper from "./components/ValueStepper/ValueStepper";
 
 const App = () => {
   return (
-    <div className="w-screen h-screen bg-neutral-900 text-neutral-100 flex items-center justify-center">
-      <div className="space-y-2">
+    <div className="w-screen h-screen bg-neutral-900 text-neutral-100 flex items-center justify-center px-4">
+      <div className="w-full max-w-md space-y-4">
         <label className="text-sm font-semibold text-neutral-300">Unit value</label>
-        <div className="w-96 bg-neutral-950 p-4 rounded-lg space-y-4">
-            <ValueStepper />
-          </div>
+        <div className="w-full bg-neutral-950 p-4 rounded-lg space-y-4">
+          <ValueStepper />
         </div>
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
